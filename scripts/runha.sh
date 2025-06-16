6254276c6a97e71c300ee78fcbae335464f57f27
LanSilence <642459901@qq.com>
2025-06-16 20:21:52 +0800

    fix: add mtools package to QEMU and dependencies installation

--- scripts/runha.sh
+++ scripts/runha.sh
@@ -24,6 +24,10 @@ mkfs.ext4 -L hassos-data ${UBUNTU_DIR}/data.ext4
 sudo mount ${UBUNTU_DIR}/data.ext4 ${UBUNTU_DIR}/rootfs/mnt/data/
 
 cat <<EOF | sudo chroot ${UBUNTU_DIR}/rootfs 
+mkdir -p /mnt/data/.homeassistant
+chown -R haos:haos /mnt/data/.homeassistant
+sudo -i -u haos
+
 cd /homeassistant
 source venv/bin/activate
 export UV_CACHE_DIR=/homeassistant/uv-cache
