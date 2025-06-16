2d241d9b0b50c287d59d00caf7066d1b01805fa9
LanSilence <642459901@qq.com>
2025-06-16 23:17:55 +0800

    fix: add sun50i-h618-k2b device tree support and update related configurations

--- scripts/runha.sh
+++ scripts/runha.sh
@@ -4,6 +4,7 @@ ROOT_DIR=$(pwd)
 UBUNTU_DIR=${ROOT_DIR}/ubuntu
 finish() {
     set +e
+    sudo unmount ${UBUNTU_DIR}/rootfs/home/haos
     sudo ${UBUNTU_DIR}/ch-mount.sh -u ${UBUNTU_DIR}/rootfs
     sudo umount ${UBUNTU_DIR}/rootfs/tmp
     sudo umount ${UBUNTU_DIR}/rootfs/homeassistant/
@@ -19,18 +20,21 @@ sudo mount -t erofs  -o loop ${UBUNTU_DIR}/ubuntu-24.02-rootfs.img ${UBUNTU_DIR}
 ${UBUNTU_DIR}/ch-mount.sh -m ${UBUNTU_DIR}/rootfs
 sudo mount -t tmpfs -o size=512M tmpfs ${UBUNTU_DIR}/rootfs/tmp
 sudo mount ${UBUNTU_DIR}/homeassistant.img ${UBUNTU_DIR}/rootfs/homeassistant/
-fallocate -l 300M ${UBUNTU_DIR}/data.ext4
+fallocate -l 500M ${UBUNTU_DIR}/data.ext4
 mkfs.ext4 -L hassos-data ${UBUNTU_DIR}/data.ext4
 sudo mount ${UBUNTU_DIR}/data.ext4 ${UBUNTU_DIR}/rootfs/mnt/data/
-
+export http_proxy=http://192.168.31.126:7890
+export https_proxy=http://192.168.31.126:7890
 cat <<EOF | sudo chroot ${UBUNTU_DIR}/rootfs 
 mkdir -p /mnt/data/.homeassistant
+mkdir -p /mnt/data/home/haos
+mount -o bind /mnt/data/home/haos /home/haos
+chown -R haos:haos /mnt/data/home/haos
 chown -R haos:haos /mnt/data/.homeassistant
 sudo -i -u haos
 
 cd /homeassistant
 source venv/bin/activate
-export UV_CACHE_DIR=/homeassistant/uv-cache
 python3.13 -m homeassistant --config /mnt/data/.homeassistant
 
 
